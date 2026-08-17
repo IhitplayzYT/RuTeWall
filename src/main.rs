11b58c29a9eda0f67deae84a78a4b2a1131ad7ea
Ihit Acharya <ihitexe@gmail.com>
2026-08-17 15:11:06 +0530

    Sarted main read loop from fifo and finished rules config file parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use std::{ error::Error, fs};
+use std::{ collections::HashSet, error::Error, fs};
 
-use crate::helper::Helper::CLI;
+use crate::{helper::Helper::CLI, rutewall::Rutewall::Rules};
+
+use std::{fs::OpenOptions,io::{self, Read},os::unix::fs::OpenOptionsExt};
+
 
 mod helper;
 mod rutewall;
@@ -17,8 +20,51 @@ fn main() -> Result<(), Box<dyn Error>>{
         panic!("Conf path is required for RuTeWall config");
     }
 
-    let conf = fs::read_to_string(&clargs.path.unwrap())?;
-    let rules = parse_conf(&conf);
+    let rules = Rules::from_path(&clargs.path.unwrap());
+    let allow_map:HashSet<String> = rules.allows.into_iter().collect();
+    let deny_map:HashSet<String> = rules.deny.into_iter().collect();
+    let allow_map:HashSet<String> = rules.fs.into_iter().map(|x| x.path).collect();
+
+
+    let fifo_path = clargs.fifo.unwrap_or("/tmp/fifo".to_string());
+
+    let c_path = std::ffi::CString::new(fifo_path.clone()).unwrap();
+    let ret = unsafe { libc::mkfifo(c_path.as_ptr(), 0o644)};
+    if ret == -1 {
+        let err = io::Error::last_os_error();
+        if err.kind() != io::ErrorKind::AlreadyExists {
+            return Err(Box::new(err));
+        }
+    }
+    let mut fifo = OpenOptions::new().read(true).custom_flags(libc::O_NONBLOCK).open(&fifo_path)?;
+
+    let mut buf = [0u8; 4096];
+    loop {
+        match fifo.read(&mut buf) {
+            Ok(0) => {std::thread::sleep(std::time::Duration::from_millis(10));}
+            Ok(n) => {
+                // TODO:
+                let tokens = String::from_utf8(buf[..n].to_vec()).unwrap().split(" ").map(|x| x.to_string()).collect::<Vec<String>>();
+                for i in &tokens{
+
+
+                }
+
+
+
+                
+
+
+
+            }
+            Err(e) if e.kind() == io::ErrorKind::WouldBlock => {std::thread::sleep(std::time::Duration::from_millis(10));}
+            Err(e) => return Err(Box::new(e)),
+        }
+    }
+    
+
+
+
 
 
 
--- src/rutewall.rs
+++ src/rutewall.rs
@@ -1,4 +1,6 @@
 pub mod Rutewall{
+    use std::{fs::{self, File}, usize};
+
 
    type Mode = u8; 
 
@@ -7,6 +9,33 @@ pub mod Rutewall{
         pub mode: Mode,
         pub path: String
     }
+
+    fn parse_perms(perms: &str) -> Mode{
+        let mut ret = 0;
+        if perms == "read"{
+            ret |= 1 << 2;
+        }else if perms == "write"{
+            ret |= 1 << 1;
+        }else if perms == "execute"{
+            ret |= 1 << 0;
+        }else{
+            if perms.contains("r"){
+                ret |= 1 << 2;
+            }else if perms.contains("w"){
+                ret |= 1 << 1;
+            }else if perms.contains("x"){
+                ret |= 1 << 0;
+            }else{}
+        }
+        ret    
+    }
+
+    impl Default for FileSystem{
+        fn default() -> Self {
+            Self { mode: 0, path: "".to_string() }
+        }
+    }
+    
 
 
     #[derive(Debug,Clone)]
@@ -17,7 +46,57 @@ pub mod Rutewall{
         pub network: bool
     }
 
-    impl Rules
+    impl Rules{
+        pub fn new() -> Self{
+            Self { allows: vec![], deny: vec![], fs: vec![], network: false }
+        }
+
+        pub fn from_path(path: &str) -> Self{
+            let conf = fs::read_to_string(path).unwrap();
+            let lines = conf.split("\n\n").map(|x| x.trim().to_string()).collect::<Vec<String>>();
+            let mut ret = Self::new();
+            for i in &lines{
+                if i.starts_with("allow"){
+                    if let Some(strt) = i.find("["){
+                        if let Some(end) = i.find("]"){
+                            assert!(strt <= end);
+                            ret.allows.append(&mut i[strt+1..end].split(",").map(|x| x.trim().to_string()).collect::<Vec<String>>());
+                        }
+                    }
+                } else if i.starts_with("deny"){
+                    if let Some(strt) = i.find("["){
+                        if let Some(end) = i.find("]"){
+                            assert!(strt <= end);
+                            ret.deny.append(&mut i[strt+1..end].split(",").map(|x| x.trim().to_string()).collect::<Vec<String>>());
+                        }
+                    }
+                } else if i.starts_with("[filesystem]") {
+                    for perm_lists in i.split("\n"){
+                        let mut mode = 0;
+                        let mut paths = vec![];
+                        let mut fs = FileSystem::default();
+                        for z in perm_lists.split("=").map(|x| x.trim()){
+                            if z.starts_with("["){
+                                if let Some(ed) = z.find("]"){
+                                    paths.append(&mut i[1..ed].split(",").map(|x| x.trim()).collect());
+                                }
+                            }else{
+                                mode = parse_perms(z);
+                            }
+                            ret.fs.append(&mut paths.iter().map(|x| FileSystem{mode,path:x.to_string()}).collect());
+                        }
+                    }
+                } else if i.starts_with("[network]"){
+                    if let Some(x) = i.find("="){
+                        ret.network = i[x+1..].to_lowercase().parse::<bool>().expect("Allow field in Network section can be only True/False");
+                    }
+                }else{}
+            }
+            ret
+        }
+
+
+    }
 
 
 
